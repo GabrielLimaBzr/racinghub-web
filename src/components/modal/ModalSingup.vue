ca6dfe25a30c0ebc65f306752a50631375196086
GabrielLimaBzr <gabriel18lb@outlook.com>
2024-01-08 16:52:19 -0300

    fix

--- src/components/modal/ModalSingup.vue
+++ src/components/modal/ModalSingup.vue
@@ -20,19 +20,11 @@
                         </VaCardTitle>
                         <VaCardContent>
                             <VaForm tag="form" @submit.prevent="handleSubmit" class="flex flex-col gap-3 w-full">
-                                <VaInput v-model="username" label="Username" />
+                                <VaInput v-model="email" label="Email" />
 
                                 <VaInput v-model="password" type="password" label="Password" />
 
-                                <VaInput v-model="password" type="password" label="Password" />
-
-                                <VaInput v-model="password" type="password" label="Password" />
-
-                                <VaInput v-model="password" type="password" label="Password" />
-
-                                <VaInput v-model="password" type="password" label="Password" />
-
-                                <VaSelect v-model="value" label="Role" :options="options" clearable />
+                                <VaInput v-model="password" type="password" label="Confirm Password" />
 
                                 <VaButton type="submit">
                                     Criar Conta
