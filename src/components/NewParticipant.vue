75ccaeff279962079a7d96bf7fad592cf2dd894d
GabrielLimaBzr <gabriel18lb@outlook.com>
2024-01-09 15:36:03 -0300

    new participant feature

---
+++ src/components/NewParticipant.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <VaForm tag="form" @submit.prevent="handleSubmit" class="flex flex-col gap-3 w-full">
+            <VaInput v-model="email" label="Email" />
+
+            <VaInput v-model="password" type="password" label="Password" />
+
+            <VaInput v-model="password" type="password" label="Confirm Password" />
+
+            <VaButton type="submit">
+                Criar Conta
+            </VaButton>
+        </VaForm>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
