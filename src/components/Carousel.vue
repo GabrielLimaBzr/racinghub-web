ae45ef0da30336d4be3dd0573decce1eb15cf117
GabrielLimaBzr <gabriel18lb@outlook.com>
2024-01-15 09:31:04 -0300

    fix mocks

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -9,9 +9,7 @@ import { ref, watchEffect } from 'vue';
 const value =  ref(0);
 const item = ["https://picsum.photos/1500",
         "https://picsum.photos/1501",
-        "https://picsum.photos/1502",
-        "https://picsum.photos/1503",
-        "https://picsum.photos/1504",]
+        "https://picsum.photos/1502",]
 
 defineProps({
     event:{
--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -20,51 +20,6 @@ export default {
     },
     data() {
         return {
-            events: [
-                {
-                    id: 1,
-                    title: "Evento 1",
-                    date: "01/01/2023",
-                    image: "caminho/para/imagem1.jpg"
-                    // Outras informações do evento aqui
-                },
-                {
-                    id: 2,
-                    title: "Evento 2",
-                    date: "02/01/2023",
-                    image: "caminho/para/imagem2.jpg"
-                    // Outras informações do evento aqui
-                },
-                {
-                    id: 2,
-                    title: "Evento 2",
-                    date: "02/01/2023",
-                    image: "caminho/para/imagem2.jpg"
-                    // Outras informações do evento aqui
-                },
-                {
-                    id: 2,
-                    title: "Evento 2",
-                    date: "02/01/2023",
-                    image: "caminho/para/imagem2.jpg"
-                    // Outras informações do evento aqui
-                },
-                {
-                    id: 2,
-                    title: "Evento 2",
-                    date: "02/01/2023",
-                    image: "caminho/para/imagem2.jpg"
-                    // Outras informações do evento aqui
-                },
-                {
-                    id: 2,
-                    title: "Evento 2",
-                    date: "02/01/2023",
-                    image: "caminho/para/imagem2.jpg"
-                    // Outras informações do evento aqui
-                },
-                // Adicione mais eventos conforme necessário
-            ]
         };
     }
 }
